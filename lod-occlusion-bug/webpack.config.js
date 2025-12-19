c788367e09e81f8fb8a734975e843a0f5e572438
Kieran Farr <kieran.farr@gmail.com>
2025-12-19 13:58:40 -0800

    save to github pages

--- lod-occlusion-bug/webpack.config.js
+++ lod-occlusion-bug/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   externals: {
     three: 'THREE'
